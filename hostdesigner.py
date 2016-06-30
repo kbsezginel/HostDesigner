418e80097b9e867a2eac6f5a6997f145fb17429d
Kutay B. Sezginel <kbs37@pitt.edu>
2016-06-30 14:13:08 -0400

    Added new functions and CrownEthers notebook.
    hostdesigner.py:
    
    - hdo_read():
      - Reads hdo output file, returns results and prints table.
      - Contains 'host' and 'xyz' information to export structures.
    
    - hdo_export():
      - Export hdo structures in different formats.
      - Currently only exports to xyz format.
      - Add export in host format option.
        - Need to decide on attachment points, drive and stuff.
    
    CrownEther.ipynb:
    
    - Trials to discover various crown ethers using HostDesigner.
    - Started with host input used in Case1, removed one of the CH3 groups.
    - Used LINKER for this input to obtain crown piece number 1.
    
      Next:
        - Use drive to change O-C-O angle for different crown ethers.
        - Adjust Li ion position.
        - Make additional LINKER runs.

--- hostdesigner.py
+++ hostdesigner.py
@@ -1,10 +1,12 @@
 import os
 
+from tabulate import tabulate
 
-def host_export(host_path, export_dir):
+def host_export(host_dir, export_dir, host='hosta'):
     """
     Convert host structure file to xyz format.
     """
+    host_path = os.path.join(host_dir, host)
     host_file = open(host_path, 'r')
     host_lines = host_file.readlines()
     host_file.close()
@@ -61,6 +63,7 @@ def linker_export(linker_path, export_dir, num=5, xyz_name=''):
         start_line += linker_structures['num_atoms'][structure] + 2
 
 
+# Change to read_drive similar to read_hdo
 def drive_export(drive_path, export_dir, num=5, xyz_name=''):
     """
     Export a given number of structures with lowest RMSD values from drive.out file.
@@ -96,7 +99,73 @@ def drive_export(drive_path, export_dir, num=5, xyz_name=''):
         start_line += drive_structures['num_atoms'][structure] + 2
 
 
-def hdo2host():
+def hdo_read(hdo_dir, num='all', table=True, hdo='out_1.hdo'):
+    """
+    Export a given number of structures with lowest RMSD values from hdo.out file.
+    """
+    hdo_path = os.path.join(hdo_dir, hdo)
+    hdo_file = open(hdo_path, 'r')
+    hdo_lines = hdo_file.readlines()
+    hdo_file.close()
+
+    num_structures = 0
+    hdo_structures = {'rmsd': [], 'energy': [], 'num_atoms': [],
+                      'info': [], 'xyz': [], 'host': [], 'linker': [], 'index': []}
+    for line_index, line in enumerate(hdo_lines):
+        if line.replace(' ', '').strip('\n').isdigit():
+            hdo_structures['num_atoms'].append(int(line.replace(' ', '').strip('\n')))
+        if 'RMSD' in line:
+            hdo_structures['rmsd'].append(float(line.split(',')[0].split('=')[-1]))
+            hdo_structures['energy'].append(float(line.split(',')[2].split('_')[1]))
+            hdo_structures['info'].append(line.strip('_'))
+            hdo_structures['linker'].append(line.strip('_').split('_')[2])
+            hdo_structures['index'].append(num_structures)
+            num_structures += 1
+
+    if num == 'all':
+        num = num_structures
+
+    start_line = 2
+    for structure in range(min(num, num_structures)):
+
+        host_lines = hdo_structures['info'][structure] + '\n'
+        host_lines += ' ' + str(hdo_structures['num_atoms'][structure]) + '\t1\n'
+        end_line = start_line + hdo_structures['num_atoms'][structure]
+        for line in hdo_lines[start_line:end_line]:
+            host_lines += line
+
+        hdo_structures['host'].append(host_lines)
+        start_line += hdo_structures['num_atoms'][structure] + 2
+
+    start_line = 2
+    for structure in range(min(num, num_structures)):
+        xyz_lines = str(hdo_structures['num_atoms'][structure]) + '\n'
+        xyz_lines += str(structure + 1) + '_' + str(hdo_structures['rmsd'][structure]) + '\n'
+
+        end_line = start_line + hdo_structures['num_atoms'][structure]
+        for line in hdo_lines[start_line:end_line]:
+            name, x, y, z = line.split()[0], line.split()[1], line.split()[2], line.split()[3]
+            xyz_lines += name + '\t' + x + '\t' + y + '\t' + z + '\n'
+
+        hdo_structures['xyz'].append(xyz_lines)
+        start_line += hdo_structures['num_atoms'][structure] + 2
+
+    if table:
+        print(tabulate({'Structure': hdo_structures['index'][:num],
+                        'Linker': hdo_structures['linker'][:num],
+                        'Num Atoms': hdo_structures['num_atoms'][:num],
+                        'RMSD': hdo_structures['rmsd'][:num],
+                        'Energy': hdo_structures['energy'][:num]}, headers="keys"))
+
+    return hdo_structures
+
+
+def hdo_export(hdo_structures, hdo_index, export_dir, format='xyz'):
+
+    xyz_path = os.path.join(export_dir, str(hdo_index) + 'hdo' + '.xyz')
+    xyz_file = open(xyz_path, 'w')
+    xyz_file.write(hdo_structures['xyz'][hdo_index])
+    xyz_file.close()
 
 # Required variables:
 # - atachment info
