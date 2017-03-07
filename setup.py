65ff75e4b5ba9fd38db860c3621eba51ad2b26cd
kbsezginel <kbs37@pitt.edu>
2017-03-07 17:49:45 -0500

    Added dependencies to requirements.txt.
    Modified setup.py to read the dependencies from this file as well.

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="hostdesigner",
     version="0.1",
     description="HostDesigner python wrapper",
     author="Kutay B. Sezginel",
     author_email="kbs37@pitt.edu",
-    install_requires=[
-        'numpy',
-        'pyyaml',
-        'tabulate',
-        'nglview'
-    ],
+    install_requires=requirements,
     include_package_data=True,
     packages=find_packages()
 )
